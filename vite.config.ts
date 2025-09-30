df5b61a61748ab59b9741dc908258a6962ccbeea
balabm <40985887+balabm@users.noreply.github.com>
2025-09-30 23:39:24 +0530

    Fix: Update base path to match repository name (leFranco)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Franco/',
+  base: '/leFranco/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
